7538234ab187dcb264e34bff6083db8d89eb712c
Hanna Yusuf <33129890+hannayusuf97@users.noreply.github.com>
2024-05-23 14:38:10 +0200

    Update unzip.lua

--- unzip.lua
+++ unzip.lua
@@ -1,10 +1,20 @@
 local files = get_input_files()
 local username = users_find()
 
+local supportedExtensions = {".zip", ".7z", ".rar", ".tar", ".gz", ".bz2", ".xz"}
+
+local function removeSupportedExtensions(fpath)
+    for _, ext in ipairs(supportedExtensions) do
+        fpath = fpath:gsub(ext .. "$", "")
+    end
+    return fpath
+end
+
 if (#files >  0) then
     for i, file in ipairs(files) do
-        local fpath = string.lower(meta_data(file).local_path)
-        local noext = fpath:gsub("%.zip$", "")
-        shell_command("unzip " .. fpath .." -d " .. noext)
+        local fpath = (meta_data(file).local_path)
+        local noext = removeSupportedExtensions(fpath)
+        shell_command("7z x " .. fpath .." -o" .. noext)
+        shell_command("chmod a+rx " .. noext)
     end
 end
