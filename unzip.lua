a01a8230841258e42e5e78d0e89fabef0b503d85
Hanna Yusuf <33129890+hannayusuf97@users.noreply.github.com>
2024-07-28 09:50:18 +0200

    Update unzip.lua
    - Added support for rar files in different languages
    - Added support for different file location using checkbox
    - Added unrar functionality which is much more stable
    - Added functionality to deal with spaces and special characters

--- unzip.lua
+++ unzip.lua
@@ -1,8 +1,12 @@
+-- Functions to get input files and find user
 local files = get_input_files()
 local username = users_find()
+local is_dirpath = get_input("is_dirpath")
 
+-- Supported extensions
 local supportedExtensions = {".zip", ".7z", ".rar", ".tar", ".gz", ".bz2", ".xz"}
 
+-- Function to remove supported extensions from file paths
 local function removeSupportedExtensions(fpath)
     for _, ext in ipairs(supportedExtensions) do
         fpath = fpath:gsub(ext .. "$", "")
@@ -10,11 +14,41 @@ local function removeSupportedExtensions(fpath)
     return fpath
 end
 
-if (#files >  0) then
+-- Function to create a directory if it doesn't exist
+local function createDirectory(dir)
+    Commented out shell command, added message for testing
+    shell_command("mkdir -p \"" .. dir .. "\"")
+end
+
+-- Function to extract files based on their extension
+local function extractFile(fpath, noext)
+    local command = ""
+    if fpath:match("%.rar$") then
+        if is_dirpath then
+            command = "unrar x \"" .. fpath .. "\" \"" .. noext .. "\""
+        else
+            command = "unrar x \"" .. fpath .. "\""
+        end
+    else
+        if is_dirpath then
+            command = "7z x \"" .. fpath .. "\" -o\"" .. noext .. "\""
+        else
+            command = "7z x \"" .. fpath .. "\""
+        end
+    end
+    add_message("Extracting file: " .. command)
+    return command
+end
+
+-- Main script logic
+if (#files > 0) then
     for i, file in ipairs(files) do
-        local fpath = (meta_data(file).local_path)
+        local fpath = meta_data(file).local_path
         local noext = removeSupportedExtensions(fpath)
-        shell_command("7z x " .. fpath .." -o" .. noext)
-        shell_command("chmod a+rx " .. noext)
+        createDirectory(noext)  -- Create directory before extraction
+        local command = extractFile(fpath, noext)
+        add_message("Command to execute: " .. command)
+        shell_command(command)
+        shell_command("chmod a+rx \"" .. noext .. "\"")
     end
 end
